f87b6533c63b1221732eecee82443efe12bf749e
Stevo1000 <nsanzestevo1@gmail.com>
2025-08-17 23:07:08 +0200

    Fix schema.py errors and complete Task 2 with working mutations

--- schema.py
+++ schema.py
@@ -1,6 +1,15 @@
 import graphene
+from crm.schema import Query as CrmQuery, Mutation as CrmMutation
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
 
-schema = graphene.Schema(query=Query)
+class Query(CrmQuery, graphene.ObjectType):
+    """Root Query"""
+    pass
+
+
+class Mutation(CrmMutation, graphene.ObjectType):
+    """Root Mutation"""
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
